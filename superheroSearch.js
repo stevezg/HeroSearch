937f6f841569aedfccec95c5257288e7c7ee37b6
Stephen Anderson <anderssj@usc.edu>
2018-11-02 19:39:22 -0700

    Which superhero is your favorite?

---
+++ superheroSearch.js
@@ -0,0 +1,77 @@
+const HEROES = [
+  { id: 1, name: 'Captain America', squad: 'Avengers' },
+  { id: 2, name: 'Iron Man', squad: 'Avengers' },
+  { id: 3, name: 'Spiderman', squad: 'Avengers' },
+  { id: 4, name: 'Superman', squad: 'Justice League' },
+  { id: 5, name: 'Wonder Woman', squad: 'Justice League' },
+  { id: 6, name: 'Aquaman', squad: 'Justice League' },
+  { id: 7, name: 'Hulk', squad: 'Avengers' }
+]
+
+function findOne(arr, query) {
+  for (const element of arr) {
+    let match = true
+
+    for (const key in query) {
+      if (query.key !== element.key) {
+        match = false
+        break
+      }
+    }
+    if (!match) {
+      continue
+    }
+    return element
+  }
+  return null
+}
+
+console.log(findOne(HEROES, { id: 1 }))
+// => { id: 1, name: 'Captain America', squad: 'Avengers' }
+
+console.log(findOne(HEROES, { id: 10 }))
+// => null
+
+console.log(findOne(HEROES, { id: 2, name: 'Aquaman' }))
+// => null
+
+console.log(findOne(HEROES, { id: 5, squad: 'Justice League' }))
+// => { id: 5, name: 'Wonder Woman', squad: 'Justice League' }
+
+console.log(findOne(HEROES, { squad: 'Justice League' }))
+// => { id: 4, name: 'Superman', squad: 'Justice League' }
+
+const Database = {
+  store: {
+    heroes: [
+      { id: 1, name: 'Captain America', squad: 'Avengers' },
+      { id: 2, name: 'Iron Man', squad: 'Avengers' },
+      { id: 3, name: 'Spiderman', squad: 'Avengers' },
+      { id: 4, name: 'Superman', squad: 'Justice League' },
+      { id: 5, name: 'Wonder Woman', squad: 'Justice League' },
+      { id: 6, name: 'Aquaman', squad: 'Justice League' },
+      { id: 7, name: 'Hulk', squad: 'Avengers' }
+    ]
+  },
+
+  findOne: function(query) {
+    for (const element of this.store.heroes) {
+      let match = true
+
+      for (const key in query) {
+        if (query[key] !== element[key]) {
+          match = false
+          break
+        }
+      }
+      if (!match) {
+        continue
+      }
+      return element
+    }
+    return null
+  }
+}
+
+console.log(Database.findOne({ id: 2 }))
+// => { id: 2, name: 'Iron Man', squad: 'Avengers' })
